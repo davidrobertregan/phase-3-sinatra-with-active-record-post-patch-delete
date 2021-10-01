524cea0dac29dbd553141e059f10600550ab7c61
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2021-10-01 11:53:27 -0500

    commit tree 73918126849e7873ca5af5f50f20434a4b69c4fa

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,10 @@
+require 'pry'
+
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
 
   get '/games' do
-    games = Game.all.order(:title).limit(10)
+    games = Game.all
     games.to_json
   end
 
@@ -14,6 +16,31 @@ class ApplicationController < Sinatra::Base
         user: { only: [:name] }
       } }
     })
+  end
+
+  delete '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.destroy
+    review.to_json
+  end
+
+  post '/reviews' do
+    review = Review.create(
+      score: params[:score],
+      comment: params[:comment],
+      game_id: params[:game_id],
+      user_id: params[:user_id]
+    )
+    review.to_json
+  end
+
+  patch '/reviews/:id' do
+    review = Review.find params[:id]
+    review.update(
+      score: params[:score],
+      comment: params[:comment]
+    )
+    review.to_json
   end
 
 end
